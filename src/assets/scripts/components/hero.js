aa8f8e0b5ce4819a6dd6af8a692f529d3f5ea33a
Nikola Nikolic <nikollanik17@gmail.com>
2022-04-06 01:16:24 +0200

    hero animations

--- src/assets/scripts/components/hero.js
+++ src/assets/scripts/components/hero.js
@@ -3,14 +3,15 @@
 import gsap from 'gsap';
 
 $(document).ready(function () {
-    const heroText = $('.hero__text');
+    // const heroText = $('.hero__text');
     const hero = $('.hero');
     const playBtn = $('.hero .play-icon');
     const videoModal = $('.video-modal');
-    // const video = document.querySelector('.video-modal video');
     const videoModalClose = $('.video-modal__close');
 
-    console.log('dwaidbwaio');
+    // const heroFirstRow = $('.hero__main-text.first-row');
+    // const heroSecondRow = $('.hero__main-text.second-row');
+    // const heroThirdRow = $('.hero__main-text.third-row');
 
     if (playBtn.length && videoModalClose.length) {
         $(playBtn).click(function () {
@@ -34,17 +35,32 @@ $(document).ready(function () {
             }
         });
 
-        tl.from(heroText, {
-            y: 30,
+        tl.from('.hero__main-text.first-row *', {
+            x: -30,
             delay: 0.1,
             autoAlpha: 0,
+            stagger: 0.3,
             ease: 'power2.out'
         });
 
-        tl.from('.hero__button', {
-            y: 30,
+        tl.from('.hero__main-text.second-row *', {
+            x: -30,
             autoAlpha: 0,
+            stagger: 0.3,
             ease: 'power2.out'
         });
+
+        tl.from('.hero__main-text.third-row *', {
+            x: -30,
+            autoAlpha: 0,
+            stagger: 0.3,
+            ease: 'power2.out'
+        });
+
+        // tl.from('.hero__button', {
+        //     y: 30,
+        //     autoAlpha: 0,
+        //     ease: 'power2.out'
+        // });
     }
 });
